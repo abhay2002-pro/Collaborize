a3ed9a47fd4b6fc8702450e2ba5c21bd28beaf86
abhay2002-pro <abhayray2002@gmail.com>
2023-01-24 23:55:52 +0530

    implemented editor

--- client/src/components/Editor.jsx
+++ client/src/components/Editor.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import { useEffect } from "react";
+import Codemirror from "codemirror";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/dracula.css";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/addon/edit/closetag";
+import "codemirror/addon/edit/closebrackets";
 
 function Editor() {
-  return (
-    <div>
-      Editor goes here
-    </div>
-  )
+  useEffect(() => {
+    async function init() {
+      Codemirror.fromTextArea(document.getElementById("realtimeEditor"), {
+        mode: { name: "javascript", json: true },
+        theme: "dracula",
+        autoCloseTags: true,
+        autoCloseBrackets: true,
+        lineNumbers: true,
+      });
+    }
+    init();
+  }, []);
+  return <textarea id="realtimeEditor"></textarea>;
 }
 
-export default Editor
+export default Editor;
